928715ecee2be5d7eace0e6c287363652ad09599
Chen Ling <chen.ling@kcl.ac.uk>
2019-02-24 18:10:12 +0000

    Create UserBeam.java

---
+++ datamanage_team/Android app/Back-end/src/Login_Signup/UserBeam.java
@@ -0,0 +1,39 @@
+package Login_Signup;
+
+public class UserBeam {
+
+	String username,useremail,password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserEmail() {
+		return useremail;
+	}
+
+	public void setUserEmail(String useremail) {
+		this.useremail = useremail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/*public String getUserImg() {
+		return userimg;
+	}
+
+	public void setUserImg(String userimg) {
+		this.userimg = userimg;
+	}*/
+
+}
